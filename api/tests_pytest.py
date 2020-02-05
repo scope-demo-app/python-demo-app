639feb1a891c8e04388b0855c78b4b8ec25a124b
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2020-02-05 17:42:15 +0100

    add lots of tests and some flaky

--- api/tests_pytest.py
+++ api/tests_pytest.py
@@ -1,4 +1,6 @@
 import time
+import random
+
 
 def increment(x):
     return x + 1
@@ -20,3 +22,319 @@ def test_benchmark(benchmark):
 def test_expensive_operation():
     time.sleep(5)
     assert True
+
+def test_function_Nedra():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Alma():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Shannon():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Hwa():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Kera():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Mathilde():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Britta():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Dorthea():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Vivienne():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Dorthey():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Tana():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Sari():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Lila():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Ed():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Jennine():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Ardell():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Alejandrina():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Ralph():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Michaele():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Estell():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Dara():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Fonda():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Johnsie():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Julia():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Catrina():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Thanh():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Ewa():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Solomon():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Candra():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Carrie():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Min():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Clarisa():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_David():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Leighann():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Cara():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Roderick():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Joline():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Gianna():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Laticia():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Ronnie():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Lavonne():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Gaylene():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Elvin():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Natalya():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Soraya():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Gustavo():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Laverna():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Ann():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Katharine():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Marlon():
+    time.sleep(random.uniform(0, 1))
+    assert True
+
+
+def test_function_Nilsa():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Ladawn():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Queenie():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Suzi():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Pearline():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Manuela():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Debra():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Jesusita():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Scarlet():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Karan():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Gail():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Francis():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Cathie():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Louisa():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Dessie():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Nichelle():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Shannon():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Laurice():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Toni():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Stephan():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Clora():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Renee():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Claudette():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Dimple():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Jinny():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Calista():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Rikki():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Caren():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Harriette():
+    time.sleep(random.uniform(0, 1))
+    assert True
+def test_function_Malcolm():
+    time.sleep(random.uniform(0, 1))
+    assert True
+
+
+
+# slow tests
+
+def test_function_Casimira():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Jolie():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Jenelle():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Lynnette():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Octavia():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Jeanine():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Rafaela():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Darcy():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Erma():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Anisa():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Aura():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Danille():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Flo():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Ingeborg():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Mahalia():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Bunny():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Charline():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Waneta():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Margaret():
+    time.sleep(random.randint(1,30))
+    assert True
+def test_function_Michiko():
+    time.sleep(random.randint(1,30))
+    assert True
+
+@pytest.mark.parametrize('execution_number', range(5))
+def test_flaky(execution_number):
+    if execution_number % 2 == 1:
+        assert True
+    else
+        assert False
+
