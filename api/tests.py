59396fd69ec35a8403615679c59c62d796e7da5e
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2020-03-26 11:27:32 +0100

    Update tests

--- api/tests.py
+++ api/tests.py
@@ -12,13 +12,14 @@ class RatingsTestCase(TestCase):
         self.restaurant_id = uuid.uuid4()
         Rating.objects.create(restaurant=self.restaurant_id, rating=1)
         Rating.objects.create(restaurant=self.restaurant_id, rating=2)
+        Rating.objects.create(restaurant=self.restaurant_id, rating=3)
         self.client = Client()
 
     def test_calculates_average(self):
         response = self.client.get(reverse('ratings', args=(self.restaurant_id,)))
         json = response.json()
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json['rating'], 1.5)
+        self.assertEqual(json['rating'], 2)
         self.assertEqual(json['restaurant'], str(self.restaurant_id))
 
 
